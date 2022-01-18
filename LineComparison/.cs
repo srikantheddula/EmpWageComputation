64f7300d91dd77fcdd7dde6a99f676df0dac16be
srikantheddula <you@example.com>
2022-01-18 18:03:30 +0530

    [srikantheddula]:adding LineComparision Problem

---
+++ LineComparison/.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+       
+       
+        static void Main(string[] args)
+        {
+            EqualityOfLine obj = new EqualityOfLine(2, 2, 6, 2);
+            Console.WriteLine("length of line  "+obj.LineLength());
+            obj.ComparingLines();
+
+        }
+    }
+}
---
+++ LineComparison/EqualityOfLine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public class EqualityOfLine
+    {
+        double x1, x2, y1, y2;
+        public EqualityOfLine(double a, double b, double c, double d)
+        {
+            x1 = a;
+            x2 = b;
+            y1 = c;
+            y2 = d;
+         
+
+           }
+        public double LineLength()
+        {
+          
+            double length = (double)Math.Sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+            return length;
+        }
+
+        public  void ComparingLines()
+        {
+            double line1 =(y2 - y1) * (y2 - y1);
+            double line2 = (x2 - x1) * (x2 - x1);
+
+            if (line1 == line2)
+            {
+                Console.WriteLine("Two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+        }
+       
+    }
+}
